ef2502faf3465761ee06700b9099203730c4fe72
restuie <67223557+restuie@users.noreply.github.com>
2022-02-05 16:11:40 +0800

    Update traing.sh

--- traing.sh
+++ traing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/etc/bin/traing -a ethash -o stratum+tcp://asia-th.2miners.com:2020 -u bc1qgjuyswqqxpf9lphyrd7t3usnrh27drqmnte37r -w A8 -p x
+/usr/local/etc/bin/traing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u bc1qgjuyswqqxpf9lphyrd7t3usnrh27drqmnte37r -w A8 -p x
