9c3582b64e369b3d0961ebccc2f165c9c7398ddc
restuie <67223557+restuie@users.noreply.github.com>
2022-02-18 14:20:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ sudo mv /usr/local/etc/$VERSION/lolMiner /usr/local/etc/bin
 sudo rm -r /usr/local/etc/$VERSION
 rm lolMiner_v$VERSION\_Lin64.tar.gz
 
-git clone https://github.com/restuie/training.git
-cd training
+git clone https://github.com/restuie/maixiang.git
+cd maixiang
 sudo chmod 777 traing.sh
 sudo chmod 644 run.service
 sudo mv traing.sh /usr/local/traing.sh
 sudo mv run.service /etc/systemd/system/run.service
 rm README.md
 cd ~/
-sudo rm -r training
+sudo rm -r maixiang
 sudo systemctl daemon-reload
