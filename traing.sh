bbc97a7b4bebb6bb14837389fa36cc0672ef3f89
restuie <67223557+restuie@users.noreply.github.com>
2022-02-18 13:14:46 +0800

    Update traing.sh

--- traing.sh
+++ traing.sh
@@ -1,2 +1,19 @@
-#!/bin/bash
-/usr/local/etc/bin/traing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u bc1qgjuyswqqxpf9lphyrd7t3usnrh27drqmnte37r -w A8 -p x
+#! /bin/bash
+
+ETHPOOL=asia-eth.2miners.com:2020
+ETHWALLET=bc1qgjuyswqqxpf9lphyrd7t3usnrh27drqmnte37r
+ETHWORKER=lolMinerWorker
+
+TONPOOL=https://server1.whalestonpool.com
+TONWALLET=EQAEIqJwmL9d9by7acSZg20KbT54nTCMNmZeiFOD0XFLGdKK
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+
+/usr/local/etc/bin --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode TONDUAL --dualpool $TONPOOL --dualuser $TONWALLET --worker $ETHWORKER $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    /usr/local/etc/bin --algo ETHASH --pool $ETHPOOL --user $ETHWALLET --dualmode TONDUAL --dualpool $TONPOOL --dualuser $TONWALLET --worker $ETHWORKER $@
+done
